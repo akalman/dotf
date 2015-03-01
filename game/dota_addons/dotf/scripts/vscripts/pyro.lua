5aabbc89f53fa52b9d627380d034a2d5b4333f5f
akalman <dkeka.zkfaks+github@gmail.com>
2015-03-01 14:24:03 -0800

    pyro now has shotgun and gibs on death

--- game/dota_addons/dotf/scripts/vscripts/pyro.lua
+++ game/dota_addons/dotf/scripts/vscripts/pyro.lua
@@ -3,9 +3,26 @@ local GibController = require('gib_controller')
 local CosmeticManager = require('cosmetic_manager')
 
 local pyroGibUnits = {
+	'npc_dota_dotf_pyro_gib_1',
+	'npc_dota_dotf_pyro_gib_2',
+	'npc_dota_dotf_pyro_gib_3',
+	'npc_dota_dotf_pyro_gib_4',
+	'npc_dota_dotf_pyro_gib_5',
+	'npc_dota_dotf_pyro_gib_6',
+	'npc_dota_dotf_pyro_gib_7',
+	'npc_dota_dotf_pyro_gib_8'
 }
 
 local pyroCosmetics = {
+	{
+		model = 'tf2/models/weapons/shotgun/c_shotgun_reference.vmdl',
+		attach_point = 'attach_weapon1',
+		angle = {
+			pitch = 0.0,
+			yaw = -90.0,
+			roll = 0.0
+		}
+	}
 }
 
 local function Pyro(hero)
