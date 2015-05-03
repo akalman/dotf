b88e40acf182910a2e179ad1f340d14f19a8654b
akalman <dkeka.zkfaks+github@gmail.com>
2015-05-03 15:22:34 -0700

    fix caching errors and add flamethrower mark 1
    add all of the used files to the cache, this allows the rocket to
    actually fire the model, since the model is cached.  Also add the
    pyros flamethrower.  Currently it fires many dragonslaves, each one
    does damage, but not DOT, also the pyro is not silenced or disarmed
    for the duration.

--- game/dota_addons/dotf/scripts/vscripts/addon_game_mode.lua
+++ game/dota_addons/dotf/scripts/vscripts/addon_game_mode.lua
@@ -11,10 +11,11 @@ function Precache( context )
 	]]
 	print("[dotf] Performing pre-load precache")
 	
-	PrecacheResource( "model_folder", "tf2/model/player/soldier", context )
+	PrecacheResource( "model_folder", "tf2/models/player/soldier", context )
+	PrecacheResource( "particle_folder", "particles/player/soldier/abilities", context )
 	PrecacheModel( 'tf2/models/weapons/pickaxe/c_pickaxe_reference.vmdl', context )
 	
-	PrecacheResource( "model_folder", "tf2/model/player/pyro", context )
+	PrecacheResource( "model_folder", "tf2/models/player/pyro", context )
 	PrecacheModel( 'tf2/models/weapons/shotgun/c_shotgun_reference.vmdl', context )
 end
 
